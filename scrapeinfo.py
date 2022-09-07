4061ac5f1106179d8cc6139cb3e3d795785d73d8
unknown <ahmedsaid2000321@gmail.com>
2022-09-07 14:06:26 +0200

    EDA'

---
+++ scrapeinfo.py
@@ -0,0 +1,24 @@
+from cgitb import html
+from bs4 import BeautifulSoup
+import pandas as pd
+
+with open('html.txt', 'r') as file:
+    html = file.read().replace('\n', '')
+
+soup = BeautifulSoup(html, 'html.parser')
+
+plant_name = []
+n_of_imgs =[]
+names_elements = soup.select('#site-content > div.sc-iJTIzp.dEsnHS > div.sc-dGHKFW.cpYUJD > div.sc-dCkZnI.ceoMyZ > div.sc-hZaSAO.iVwyYi > div > div.sc-eHAsqE.ginGnF > div > div.sc-gcFSfr.gquipn > div.sc-dTbhCw.eeugBO > div > div.sc-IAann.fLFzeF > p')
+files_elements = soup.select('#site-content > div.sc-iJTIzp.dEsnHS > div.sc-dGHKFW.cpYUJD > div.sc-dCkZnI.ceoMyZ > div.sc-hZaSAO.iVwyYi > div > div.sc-eHAsqE.ginGnF > div > div.sc-gcFSfr.gquipn > div.sc-dTbhCw.eeugBO > div > div.sc-IAann.fLFzeF > span')
+for i in range(len(names_elements)):
+    plant_name.append(names_elements[i].text)
+    n_of_imgs.append(files_elements[i].text)
+
+information = {
+    'plant name':plant_name,
+    'number of images':n_of_imgs,
+}
+df = pd.DataFrame(information)
+df.to_excel('datasetinfo.xlsx',index=False)
+
